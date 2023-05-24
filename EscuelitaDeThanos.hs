11d7a4b3bc6c1bce2ce6921cfe209aa49de53d36
Facu <fbovehernandez@frba.utn.edu.ar>
2023-05-24 20:31:45 -0300

    Falta el punto 5

--- EscuelitaDeThanos.hs
+++ EscuelitaDeThanos.hs
@@ -14,7 +14,7 @@ data Guantelete = UnGuantelete {
 }
 
 type Gemas = Personaje -> Personaje
-
+type Enemigo = Personaje
 type Universo = [Personaje]
 
 chasquido :: Universo -> Guantelete -> Universo
@@ -40,26 +40,26 @@ sumarEnergia  = sum . map energia . filter ((>1) . length .habilidades)
 
 -- SEGUNDA PARTE .. 
 
-laMente :: Int -> Gemas -- (Int -> Unpersonaje ) -> UnPersonaje
-laMente = sacarEnergia -- valor y unPersonaje
+gemaMente :: Int -> Gemas -- (Int -> Unpersonaje ) -> UnPersonaje
+gemaMente = sacarEnergia -- valor y unPersonaje
 
 sacarEnergia :: Int -> Personaje -> Personaje
 sacarEnergia valor unPersonaje = unPersonaje {energia = energia unPersonaje - valor}
  
-elAlma :: String -> Gemas
-elAlma habilidad  = sacarHabilidad habilidad . sacarEnergia 10 
+gemaAlma :: String -> Gemas
+gemaAlma habilidad  = sacarHabilidad habilidad . sacarEnergia 10 
 
 sacarHabilidad :: String -> Personaje -> Personaje
 sacarHabilidad habilidad unPersonaje = unPersonaje {habilidades = filter (/= habilidad) (habilidades unPersonaje)} -- e1
 
-elEspacio :: String -> Personaje -> Personaje
-elEspacio planeta = cambiarPlaneta planeta . sacarEnergia 20
+gemaEspacio :: String -> Personaje -> Personaje
+gemaEspacio planeta = cambiarPlaneta planeta . sacarEnergia 20
 
 cambiarPlaneta :: String -> Personaje -> Personaje 
 cambiarPlaneta planeta unPersonaje = unPersonaje {planeta = planeta}
 
-elPoder :: Personaje -> Personaje
-elPoder unPersonaje  
+gemaPoder :: Personaje -> Personaje
+gemaPoder unPersonaje  
     | tienePocasHabilidades unPersonaje  = (sacarEnergia (energia unPersonaje) . vaciarHabilidades) unPersonaje
     | otherwise = sacarEnergia (energia unPersonaje)  unPersonaje
 
@@ -69,15 +69,38 @@ vaciarHabilidades unPersonaje = unPersonaje {habilidades = []} -- e2
 tienePocasHabilidades :: Personaje -> Bool
 tienePocasHabilidades = (>2) . length . habilidades
 
-elTiempo :: Personaje -> Personaje 
-elTiempo = reducirTiempo . sacarEnergia 50
+gemaTiempo :: Personaje -> Personaje 
+gemaTiempo = reducirTiempo . sacarEnergia 50
 
 reducirTiempo :: Personaje -> Personaje 
 reducirTiempo unPersonaje = unPersonaje {edad = max (div (edad unPersonaje) 2) 18} 
 
-laGemaLoca :: Gemas -> Personaje -> Personaje
-laGemaLoca gema unPersonaje = gema $ gema unPersonaje 
+gemaLoca :: Gemas -> Personaje -> Personaje
+gemaLoca gema unPersonaje = gema $ gema unPersonaje 
                         --  = gema . gema
 
 implementarGemas :: Personaje -> [Gemas] -> Personaje 
 implementarGemas = foldr ($) 
+
+-- 4
+
+guanteleteGoma :: Guantelete
+guanteleteGoma = UnGuantelete "Goma" [gemaTiempo, gemaAlma "usar Mjolnir", gemaLoca (gemaAlma "programacion en Haskell")] 
+
+-- 5
+
+-- UTILIZAR es la funcion implementarGemas pero con un set nuevo de gemas para ejecutar sobre el personaje, cuando yo aplico este conjunto de gemas se genera un nuevo personaje, y al que, gracias a la funcion FOLD, se le vuelve a aplicar otra gema (de la lista de gemas) y asi hasta que no queden gemas, por eso la funcion utilizada por fold es justamente el ($), que obvia los parentesis a la hora de "aplicar" algo.
+
+-- 6
+
+
+
+
+-- 7
+
+punisher :: Personaje
+punisher = UnPersonaje 1 "Punisher" "Neptuno" 50 ["Matar a Hulk, Usar la espada"]
+
+-- No va a funcionar, ya que va a quedarse evaluar infinitamente que gema produce la perdida.  
+
+-- va a aplicar las 3 primeras gemas de la lista sobre el personaje, sin importar que sea una lista infinita. Gracias al "lazy evaluation" , haskell analiza solo aquello que necsita
