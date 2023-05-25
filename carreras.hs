0563afd6a05c5af89d558f1cac08bfe0bab9f7fd
Facu <fbovehernandez@frba.utn.edu.ar>
2023-05-25 15:50:50 -0300

    agrego mod y hasta el 3b inclusive

--- carreras.hs
+++ carreras.hs
@@ -1,13 +1,12 @@
------------------  PARCIAL SAMURAI 2016 ----------------- 1
+-----------------  PARCIAL CARRERAS 2021 ----------------- 1
 
 data Auto = UnAuto {
     color :: String,
     velocidad :: Int,
     distancia :: Int,
-    posicion :: Int -- Totalmente puesta en duda (para el punto 1c)
-   -- powerUps :: [String] -- Lista de powerUps
-} deriving (Eq,Show)
-
+    posicion :: Int -- Puede ser cualquier (mirar 1c)
+} 
+type PowerUp = [Auto] -> [Auto]
 type Carrera = [Auto]
 
 -- 1
@@ -22,20 +21,39 @@ distanciaEntreAutos auto1 auto2 = abs(distancia auto1 - distancia auto2)
 
 vaPrimero :: Auto -> Carrera -> Bool
 vaPrimero _ [] = error "No hay autos corriendo"
-vaPrimero auto (x:xs) = distancia auto > distancia x && (not . estaCerca auto) x && vaPrimero auto (auto:xs)
+vaPrimero auto (x:xs) = (not. estaDetras auto) x && (not . estaCerca auto) x && vaPrimero auto (auto:xs)
 
--- c No se como plantearlo... 
---puesto :: Auto -> Carrera -> Int
---puesto _ [] = error "No hay autos corriendo"
---puesto auto (x:xs) 
-  --  | distanciaEntreAutos auto x > 0 = modificarPosicion --auto && puesto auto xs
-   -- | otherwise = posicion auto
+modificarPosicion :: Int -> Auto -> [Auto] -> Auto
+modificarPosicion puestos unAuto lista = unAuto {posicion = 1+ cantidadAutosDelante unAuto lista}
 
-modificarPosicion :: Auto -> Auto
-modificarPosicion unAuto = unAuto {posicion = posicion unAuto + 1 }
+cantidadAutosDelante :: Auto -> [Auto] -> Int
+cantidadAutosDelante unAuto = length . filter (not . estaDetras unAuto) 
 
 -- 2
 correr :: Int -> Auto -> Auto
 correr tiempo unAuto = unAuto {distancia = distancia unAuto + tiempo * velocidad unAuto }
 
 -- b
+aplicarModificador :: (Int -> Int) -> Auto -> Auto
+aplicarModificador f unAuto = unAuto {velocidad = (f  . velocidad) unAuto }
+
+bajarVelocidad :: Int -> Auto -> Auto
+bajarVelocidad cantidad = aplicarModificador (negate . (+ cantidad))
+
+restar :: Int -> Int -> Int
+restar valor1 valor2 = min (valor2 - valor1) 0
+
+-- 3
+afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+afectarALosQueCumplen criterio efecto lista
+  = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
+
+terremoto :: Auto ->  PowerUp 
+terremoto unAuto = afectarALosQueCumplen (estaCerca unAuto) (aplicarModificador (negate . (+50)))
+
+miguelitos :: Int -> Auto -> PowerUp -- [Auto] -> [Auto]
+miguelitos cantidad unAuto = afectarALosQueCumplen (estaDetras unAuto) (aplicarModificador (negate . (+cantidad)))
+
+estaDetras :: Auto -> Auto -> Bool
+estaDetras unAuto1 unAuto2 = distancia unAuto1 > distancia unAuto2
+
